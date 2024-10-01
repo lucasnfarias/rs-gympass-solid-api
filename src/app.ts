cd1a6b090012d6c3f321605e93921c8dce6e5b51
Lucas Farias <lucas.farias@rtbhouse.com>
2024-09-30 21:58:04 -0300

    improve error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
+import { env } from '@/env'
 import { appRoutes } from '@/http/routes'
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      details: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO should log to an external tool e.g. Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
