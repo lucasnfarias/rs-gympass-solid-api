0009bf33aca3442cf72ebc42c5b342f740c4169d
Lucas Farias <lucas.farias@rtbhouse.com>
2024-10-06 18:24:42 -0300

    create jwt authentication and user profile route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import { env } from '@/env'
 import { appRoutes } from '@/http/routes'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,13 @@
 import { authenticate } from '@/http/controllers/authenticate'
+import { profile } from '@/http/controllers/profile'
 import { register } from '@/http/controllers/register'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  /* Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
